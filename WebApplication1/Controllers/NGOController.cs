d9020be5313f4d9b666326f743f6b30a568a278d
Paul Tomal <Paul Tomal@DESKTOP-6H5N4R8>
2023-03-22 00:51:22 +0600

    Add project files.

---
+++ WebApplication1/Controllers/NGOController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.EF;
+
+namespace WebApplication1.Controllers
+{
+    public class NGOController : Controller
+    {
+        public ZeroHungerEntities db = new ZeroHungerEntities();
+
+        
+        public ActionResult CollectRequest()
+        {
+            return View(db.Restaurant_Table.ToList());
+        }
+
+      
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant_Table rt = db.Restaurant_Table.Find(id);
+            if (rt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rt);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Restaurant_Table restaurant_Table = db.Restaurant_Table.Find(id);
+            db.Restaurant_Table.Remove(restaurant_Table);
+            db.SaveChanges();
+            return RedirectToAction("CollectRequest");
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/RestaurantController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.EF;
+
+namespace WebApplication1.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public ZeroHungerEntities db = new ZeroHungerEntities();
+
+        
+        public ActionResult Index()
+        {
+            return View(db.Restaurant_Table.ToList());
+        }
+
+       
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant_Table rt = db.Restaurant_Table.Find(id);
+            if (rt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rt);
+        }
+
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Restaurant_ID,Restaurant_Name,Address,Contact")] Restaurant_Table rt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurant_Table.Add(rt);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rt);
+        }
+
+        
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant_Table rt = db.Restaurant_Table.Find(id);
+            if (rt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rt);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Restaurant_ID,Restaurant_Name,Address,Contact")] Restaurant_Table rt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rt).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rt);
+        }
+
+        
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant_Table rt = db.Restaurant_Table.Find(id);
+            if (rt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rt);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Restaurant_Table restaurant_Table = db.Restaurant_Table.Find(id);
+            db.Restaurant_Table.Remove(restaurant_Table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
